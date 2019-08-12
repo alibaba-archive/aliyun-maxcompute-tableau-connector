ae1bbbb1524b2a56eac7dd1f29d6f976d684c830
jon <wangzhong.zw@alibaba-inc.com>
2019-08-12 18:16:44 +0800

    dialog box supports username and passowrd

--- plugins/maxcompute_jdbc/connectionBuilder.js
+++ plugins/maxcompute_jdbc/connectionBuilder.js
@@ -1,28 +1,12 @@
 (function dsbuilder(attr) {
+  var username = attr["username"];
+  var password = attr["password"];
+  var connectionString = attr["server"];
 
-  return [attr["server"]];
+  if (!connectionString.endsWith("&")) {
+    connectionString += "&";
+  }
 
-  // var endpoint = "https://service.odps.aliyun.com/api";
-  // if (attr["server"].startsWith("https://") || attr["server"].startsWith("http://")) {
-  //   endpoint = attr["server"];
-  // } else {
-  //   endpoint = "https://" + attr["server"];
-  // }
-
-  // var urlBuilder = "jdbc:odps:" + endpoint + (endpoint.includes("?") ? "&" : "?");
-
-  // var params = [];
-  // params["project"] = attr["dbname"];
-  // params["accessId"] = attr["username"];
-  // params["accessKey"] = attr["password"];
-
-  // var formattedParams = [];
-
-  // for (var key in params) {
-  //   formattedParams.push(connectionHelper.formatKeyValuePair(key, params[key]));
-  // }
-
-  // urlBuilder += formattedParams.join("&")
-
-  // return [urlBuilder];
+  var params = ["accessId=" + username, "accessKey=" + password];
+  return [connectionString + params.join("&")];
 })
